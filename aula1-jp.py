4176246093713e07e8b68573f39c3da1fadb9a92
JoaoPedro1808 <161887587+JoaoPedro1808@users.noreply.github.com>
2024-03-01 17:29:00 -0300

    Add files via upload

---
+++ aula1-jp.py
@@ -0,0 +1,89 @@
+"""
+Este e um comentario
+varias linhas
+
+Docsstring - testo de documentação
+
+progmação - texto de documentação
+
+Progmação estruturada
+-documentando codigo
+-saida de dados
+-tipos de dados
+-entrada de dados
+-variaveis
+"""
+# Este e um comentario de codigo.
+# fazemos uma marcação com("#") para cadalinha.
+print("hello world") # exibe uma informação na tela.
+
+# Saida de dados
+print("hello world", end=" ")
+print("hello,","world")
+print("Ana tem", 22, "anos", sep="--", )
+
+# Tipos de dados primitivos
+print(4, 7, -10, 2)             # int
+print(4.2, 5.0, 4.3, -87.8)     # float
+print(True, False)              # bool
+print("abc", "def")             # str
+print(None)                     # NonenType
+
+print(2)
+print(2.0)
+print("2")
+
+# São diferentes entre sí.
+
+print("hello world")
+
+# ou
+
+print('hello world')
+
+# caracteristicas de escape - escape char(\)
+
+print("hello \"world\"")
+
+#o escape indica um caracter que o phyton não deve tomar a serio.
+
+#podendo escrever um caracter com (" ")
+
+print("C:\\Projetos")
+
+print("""Este e um texto
+de multiplas linhas.""")
+print('''Este e um texto
+de multiplas linhas''')
+
+# Lendo dados de usuarios
+
+# O input serve para interagir com o usuario
+
+# Variáveis
+
+nome = input("informe o seu nome: ")
+print("hello," , nome)
+
+nome = "joazinho"
+print(nome)
+
+# o "nome" e uma atribuição onde o codigo guarda a informação no sistema.
+
+# Phyton é uma linguagem de tipagem fraca e dinámica, ou seja
+#não precisa expecificar o tipo do dado para rodar a função.
+
+# Não existe contraste em Phyton
+
+pi = 3.1415
+pi = "hello world"
+
+# Convenção de nomeação de variaveis: snake_case
+
+#Pro phyton e tudo a mesma coisa, por exemplo:
+pi = 3.14
+PI = 3.14
+#Quando o progamador escreve em maisculas e por que ele não quer que seja modificado.
+
+
+
